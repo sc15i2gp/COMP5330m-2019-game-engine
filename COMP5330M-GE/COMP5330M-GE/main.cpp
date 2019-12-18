146ade311276cdc5d99d13c688dda27985f47797
sc15i2gp <sc15i2gp@leeds.ac.uk>
2019-12-18 02:08:23 +0000

    Added gitignore and removed now ignored files from repo.

--- COMP5330M-GE/COMP5330M-GE/main.cpp
+++ COMP5330M-GE/COMP5330M-GE/main.cpp
@@ -102,7 +102,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmd_li
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (void*)0);
 		glEnableVertexAttribArray(0);
-		glBindVertexArray(0);
+		glBindVertexArray(0);b n 
 
 		Matrix4x4 model = identity();
 		translate(model, Vector3(0.0f, 0.2f, 0.0f));
