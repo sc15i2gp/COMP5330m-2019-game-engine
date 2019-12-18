6cb25c9330aa9fbd77106d8e3562998d43b8e992
sc15i2gp <sc15i2gp@leeds.ac.uk>
2019-12-18 02:53:09 +0000

    Added Landscape module header.

---
+++ COMP5330M-GE/COMP5330M-GE/Landscape.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//Goal: Render a procedurally generated landscape
+//Landscape:
+//	- Terrain
+//	- Trees
+
+//Terrain:
+//	- Load an array of regularly spaced vertices in a grid
+//	- Use perlin noise to set their y values
