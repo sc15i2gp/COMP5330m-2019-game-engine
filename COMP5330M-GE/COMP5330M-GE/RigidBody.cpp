d3a3a1cf32f354a722c6795be194832a2eeabb1e
Jake White <sc16jsw@leeds.ac.uk>
2020-01-28 11:55:54 +0000

    Made the rigid body dynamics independent of the meshes from the Graphics file.

--- COMP5330M-GE/COMP5330M-GE/RigidBody.cpp
+++ COMP5330M-GE/COMP5330M-GE/RigidBody.cpp
@@ -5,62 +5,22 @@ RigidBody::RigidBody() {
 	velocity = { 0.0,0.0,0.0 };
 	acceleration = { 0.0,0.0,0.0 };
 	mass = 1.0;
-	minx = -1.0;
-	maxx = 1.0;
-	miny = -1.0;
-	maxy = 1.0;
-	minz = -1.0;
-	maxz = 1.0;
+	radius = 1.0;
 }
 
-RigidBody::RigidBody(Mesh mesh, Vector3 initialVelocity, Vector3 initialAcceleration, float mass) {
-	this->mesh = mesh;
-	Vector3 verticesSum = { 0.0,0.0,0.0 };
-	minx = maxy = miny = maxx = minz = maxz = 0.0;
-	for (int i = 0; i < mesh.number_of_vertices; ++i) {
-		verticesSum += mesh.vertices[i].position;
-		if (mesh.vertices[i].position.x > maxx) maxx = mesh.vertices[i].position.x;
-		if (mesh.vertices[i].position.y > maxy) maxy = mesh.vertices[i].position.y;
-		if (mesh.vertices[i].position.z > maxz) maxz = mesh.vertices[i].position.z;
-		if (mesh.vertices[i].position.x < minx) minx = mesh.vertices[i].position.x;
-		if (mesh.vertices[i].position.y < miny) miny = mesh.vertices[i].position.y;
-		if (mesh.vertices[i].position.z < minz) minz = mesh.vertices[i].position.z;
-	}
-	verticesSum /= mesh.number_of_vertices;
-	displacement = verticesSum;
+RigidBody::RigidBody(Vector3 initialDisplacement, Vector3 initialVelocity, Vector3 initialAcceleration, float mass, float radius) {
+	displacement = initialDisplacement;
 	velocity = initialVelocity;
 	acceleration = initialAcceleration;
 	this->mass = mass;
+	this->radius = radius;
 }
 
-bool cylinderCollision(RigidBody& r, Mesh mesh) {
-	for (int i = 0; i < mesh.number_of_vertices; ++i) {
-		float x1 = mesh.vertices[i].position.x - r.maxx;
-		float y1 = mesh.vertices[i].position.y - r.maxy;
-		float z1 = mesh.vertices[i].position.z - r.maxz;
-		float x2 = r.minx - mesh.vertices[i].position.x;
-		float y2 = r.miny - mesh.vertices[i].position.y;
-		float z2 = r.minz - mesh.vertices[i].position.z;
-		if (x1 <= 0.0 || y1 <= 0.0 || z1 <= 0.0 || x2 <= 0.0 || y2 <= 0.0 || z2 <= 0.0) {
-			return true;
-		}
-	}
-	return false;
-}
-
-void updateVectorPositions(RigidBody& r, Vector3 difference)
-{
-	for (int i = 0; i < r.mesh.number_of_vertices; ++i) {
-		r.mesh.vertices[i].position += difference;
-	}
-}
-
-void updateDisplacement(RigidBody& r, Vector3* forces, int numOfForces, float timeStep) {
+Vector3 updateDisplacement(RigidBody& r, Vector3* forces, int numOfForces, float timeStep) {
 	Vector3 initialDis = r.displacement;
 	r.displacement = r.displacement + (timeStep * r.velocity) + ((0.5 * timeStep * timeStep) * r.acceleration);
 	Vector3 finalDis = r.displacement;
 	Vector3 disDiff = finalDis - initialDis;
-	updateVectorPositions(r, disDiff);
 	r.velocity = r.velocity + (0.5 * timeStep * r.acceleration);
 	Vector3 totalForce = { 0.0,0.0,0.0 };
 	for (int i = 0; i < numOfForces; ++i) {
@@ -68,4 +28,13 @@ void updateDisplacement(RigidBody& r, Vector3* forces, int numOfForces, float ti
 	}
 	r.acceleration = totalForce / r.mass;
 	r.velocity = r.velocity + (0.5 * timeStep * r.acceleration);
+	return disDiff;
+}
+
+bool checkSphereCylinderCollision(RigidBody& r, float x, float z, float miny, float maxy, float radius) {
+	if (miny <= r.displacement.y <= maxy) {
+		Vector3 m = { x, r.displacement.y, z };
+		Vector3 distanceBetweenCenters = m - r.displacement;
+		return length(distanceBetweenCenters) <= r.radius + radius;
+	}
 }
--- COMP5330M-GE/COMP5330M-GE/RigidBody.h
+++ COMP5330M-GE/COMP5330M-GE/RigidBody.h
@@ -1,31 +1,25 @@
 #pragma once
 
 #include "Maths.h"
-#include "Graphics.h"
 
 struct RigidBody
 {
-	Mesh mesh;
 	// Displacement of an object is determined by its centre of mass
 	Vector3 displacement;
 	Vector3 velocity;
 	Vector3 acceleration;
 	float mass;
 
-	// Min and max dimensions of the body - for collisions
-	float minx;
-	float maxx;
-	float miny;
-	float maxy;
-	float minz;
-	float maxz;
+	// The rigid body is a sphere, so properties need to be defined
+	float radius;
 
 	RigidBody();
-	RigidBody(Mesh mesh, Vector3 initialVelocity, Vector3 initialAcceleration, float mass);
+	RigidBody(Vector3 initialDisplacement, Vector3 initialVelocity, Vector3 initialAcceleration, float mass, float radius);
 };
 
-bool cylinderCollision(RigidBody&, Mesh mesh);
+// Calculate the amount of displacement to translate in a frame
+// Velocity verlet is being used here
+Vector3 updateDisplacement(RigidBody&, Vector3* forces, int numOfForces, float timeStep);
 
-// Update position of centre of rigid body
-void updateDisplacement(RigidBody&, Vector3* forces, int numOfForces, float timeStep);
-void updateVectorPositions(RigidBody&, Vector3 difference);
+// Collision detection
+bool checkSphereCylinderCollision(RigidBody&, float x, float z, float miny, float maxy, float radius);
