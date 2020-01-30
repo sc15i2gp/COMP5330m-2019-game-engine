8e702ae09c1451d1fff569c0ef9dece1e12f5d46
Jake White <sc16jsw@leeds.ac.uk>
2020-01-30 16:53:24 +0000

    Finished the collision function.

--- COMP5330M-GE/COMP5330M-GE/RigidBody.cpp
+++ COMP5330M-GE/COMP5330M-GE/RigidBody.cpp
@@ -1,5 +1,6 @@
 #include "RigidBody.h"
 
+// Default constructor
 RigidBody::RigidBody() {
 	displacement = { 0.0,0.0,0.0 };
 	velocity = { 0.0,0.0,0.0 };
@@ -8,6 +9,7 @@ RigidBody::RigidBody() {
 	radius = 1.0;
 }
 
+// Parameterised constructor
 RigidBody::RigidBody(Vector3 initialDisplacement, Vector3 initialVelocity, Vector3 initialAcceleration, float mass, float radius) {
 	displacement = initialDisplacement;
 	velocity = initialVelocity;
@@ -16,6 +18,8 @@ RigidBody::RigidBody(Vector3 initialDisplacement, Vector3 initialVelocity, Vecto
 	this->radius = radius;
 }
 
+// Update the displacement, velocity, and acceleration using Velocity Verlet
+// We return the difference in displacement made to translate the mesh
 Vector3 updateDisplacement(RigidBody& r, Vector3* forces, int numOfForces, float timeStep) {
 	Vector3 initialDis = r.displacement;
 	r.displacement = r.displacement + (timeStep * r.velocity) + ((0.5 * timeStep * timeStep) * r.acceleration);
@@ -32,9 +36,30 @@ Vector3 updateDisplacement(RigidBody& r, Vector3* forces, int numOfForces, float
 }
 
 bool checkSphereCylinderCollision(RigidBody& r, float x, float z, float miny, float maxy, float radius) {
+	// Check for collision with the side of the cylinder
 	if (miny <= r.displacement.y <= maxy) {
 		Vector3 m = { x, r.displacement.y, z };
 		Vector3 distanceBetweenCenters = m - r.displacement;
+		/* If the distance between the centres of the shapes is <=
+			the sum of their radii, then they are colliding
+		*/
 		return length(distanceBetweenCenters) <= r.radius + radius;
+	}
+	// Check for collision with the top of the cylinder
+	else if (r.displacement.y > maxy) {
+		Vector3 cylinderTopCentre = { x, maxy, z };
+		// Suppose there is a line parallel to the y-axis passing the centre of the sphere 
+		Vector3 lineCrossesPlane = { r.displacement.x, maxy, r.displacement.z };
+		Vector3 distanceBetween = cylinderTopCentre - lineCrossesPlane;
+		// If the sphere is passing the top of the cylinder and is in the plane of the cylinder
+		return r.displacement.y - r.radius <= maxy && length(distanceBetween) <= radius;
+	}
+	// Check for collision with the bottom of the cylinder
+	else if (r.displacement.y < miny) {
+		Vector3 cylinderBottomCentre = { x, miny, z };
+		Vector3 lineCrossesPlane = { r.displacement.x, miny, r.displacement.z };
+		Vector3 distanceBetween = cylinderBottomCentre - lineCrossesPlane;
+		// If the sphere is passing the bottom of the cylinder and is in the plane of the cylinder
+		return r.displacement.y + r.radius >= miny && length(distanceBetween) <= radius;
 	}
 }
--- COMP5330M-GE/COMP5330M-GE/RigidBody.h
+++ COMP5330M-GE/COMP5330M-GE/RigidBody.h
@@ -13,6 +13,7 @@ struct RigidBody
 	// The rigid body is a sphere, so properties need to be defined
 	float radius;
 
+	// Constructors
 	RigidBody();
 	RigidBody(Vector3 initialDisplacement, Vector3 initialVelocity, Vector3 initialAcceleration, float mass, float radius);
 };
@@ -22,4 +23,6 @@ struct RigidBody
 Vector3 updateDisplacement(RigidBody&, Vector3* forces, int numOfForces, float timeStep);
 
 // Collision detection
+// Position (x,z) defines the position of the cylinder
+// miny and maxy define the height of the cylinder
 bool checkSphereCylinderCollision(RigidBody&, float x, float z, float miny, float maxy, float radius);
