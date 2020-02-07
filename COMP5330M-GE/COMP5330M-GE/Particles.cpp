168e5482126e21f15225221117af15ea9401d9c4
Jake White <sc16jsw@leeds.ac.uk>
2020-02-07 13:54:00 +0000

    Completed the particle release function. Added other functions and gave particle rigid body mechanics.

--- COMP5330M-GE/COMP5330M-GE/Particles.cpp
+++ COMP5330M-GE/COMP5330M-GE/Particles.cpp
@@ -1,28 +1,40 @@
 #include "Particles.h"
 
-Particle::Particle() {
+Particle::Particle() 
+{
 	displacement = { 0.0, 0.0, 0.0 };
 	velocity = { 0.0, 0.0, 0.0 };
 	life = 60;
 }
 
-Particle::Particle(Vector3 initialDisplacement, Vector3 initialVelocity, int life) {
+Particle::Particle(Vector3 initialDisplacement, Vector3 initialVelocity, int life) 
+{
 	displacement = initialDisplacement;
 	velocity = initialVelocity;
 	this->life = life;
 }
 
-Emitter::Emitter() {
+ParticleBody::ParticleBody(Particle& p, Vector3 acceleration, float mass)
+{
+	particle = p;
+	this->acceleration = acceleration;
+	this->mass = mass;
+}
+
+Emitter::Emitter()
+{
 	position = { 0.0, 0.0, 0.0 };
 	radius = 0.0;
 	normalSpeedVector = { 0.0, 1.0, 0.0 };
 	rateOfParticleRelease = 10;
-	maxAngleAroundX = maxAngleAroundY = maxAngleAroundZ = 1.0;
+	maxAngleAroundX = maxAngleAroundY = maxAngleAroundZ = 30.0;
+	minSpeedRatio = 0.5;
 	minLife = 60;
 	maxLife = 500;
 }
 
-Emitter::Emitter(Vector3 position, float radius, Vector3 normalSpeedVector, float rate, float maxAngleAroundX, float maxAngleAroundY, float maxAngleAroundZ, int minLife, int maxLife) {
+Emitter::Emitter(Vector3 position, float radius, Vector3 normalSpeedVector, float rate, float maxAngleAroundX, float maxAngleAroundY, float maxAngleAroundZ, float minSpeedRatio, int minLife, int maxLife)
+{
 	this->position = position;
 	this->radius = radius;
 	this->normalSpeedVector = normalSpeedVector;
@@ -30,11 +42,13 @@ Emitter::Emitter(Vector3 position, float radius, Vector3 normalSpeedVector, floa
 	this->maxAngleAroundX = maxAngleAroundX;
 	this->maxAngleAroundY = maxAngleAroundY;
 	this->maxAngleAroundZ = maxAngleAroundZ;
+	this->minSpeedRatio = minSpeedRatio;
 	this->minLife = minLife;
 	this->maxLife = maxLife;
 }
 
-Particle releaseOneParticle(Emitter& e) {
+Particle releaseOneParticle(Emitter& e) 
+{
 	// Generate a position
 	float p1 = (((static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2) - 1) * e.radius;
 	float p2 = (((static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2) - 1) * e.radius;
@@ -44,8 +58,57 @@ Particle releaseOneParticle(Emitter& e) {
 	float xAngle = (((static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2) - 1) * e.maxAngleAroundX;
 	float yAngle = (((static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2) - 1) * e.maxAngleAroundY;
 	float zAngle = (((static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) * 2) - 1) * e.maxAngleAroundZ;
-	Vector3 vel = { 0.0, 0.0, 0.0 };
+	// Rotate around the x-axis
+	Vector3 vel = { e.normalSpeedVector.x, 
+		(e.normalSpeedVector.y * cos_deg(xAngle)) - (e.normalSpeedVector.z * sin_deg(xAngle)), 
+		(e.normalSpeedVector.y * sin_deg(xAngle)) + (e.normalSpeedVector.z * cos_deg(xAngle)) };
+	// Rotate around the y-axis
+	vel = { (vel.x * cos_deg(yAngle)) + (vel.z * sin_deg(yAngle)),
+		vel.y,
+		(-vel.x * sin_deg(yAngle)) + (vel.z * cos_deg(yAngle)) };
+	// Rotate around the z-axis
+	vel = { (vel.x * cos_deg(zAngle)) - (vel.y * sin_deg(zAngle)),
+		(vel.x * sin_deg(zAngle)) + (vel.y * cos_deg(zAngle)),
+		vel.z };
+	// Multiply the speed by a random value between minSpeedRatio and 1.0
+	float ratio = e.minSpeedRatio + (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1 - e.minSpeedRatio))));
+	vel = ratio * vel;
 	// Generate a lifespan
-	float life = e.minLife + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (e.maxLife - e.minLife)));
+	float life = e.minLife + (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (e.maxLife - e.minLife))));
 	return Particle(pos,vel,life);
+}
+
+Particle simpleReleaseOneParticle(Emitter& e)
+{
+	return Particle(e.position, e.normalSpeedVector, e.maxLife);
+}
+
+ParticleBody makeParticleRigidBody(Particle& p, Vector3 acceleration, float mass)
+{
+	return ParticleBody(p, acceleration, mass);
+}
+
+Vector3 updateParticlePosition(Particle& p, float timeStep)
+{
+	Vector3 initialDis = p.displacement;
+	p.displacement += timeStep * p.velocity;
+	Vector3 finalDis = p.displacement;
+	Vector3 diffDis = finalDis - initialDis;
+	return diffDis;
+}
+
+Vector3 updateParticlePositionUnderForces(ParticleBody& p, Vector3* forces, int numOfForces, float timeStep)
+{
+	Vector3 initialDis = p.particle.displacement;
+	p.particle.displacement = p.particle.displacement + (timeStep * p.particle.velocity) + ((0.5 * timeStep * timeStep) * p.acceleration);
+	Vector3 finalDis = p.particle.displacement;
+	Vector3 disDiff = finalDis - initialDis;
+	p.particle.velocity = p.particle.velocity + (0.5 * timeStep * p.acceleration);
+	Vector3 totalForce = { 0.0,0.0,0.0 };
+	for (int i = 0; i < numOfForces; ++i) {
+		totalForce += forces[i];
+	}
+	p.acceleration = totalForce / p.mass;
+	p.particle.velocity = p.particle.velocity + (0.5 * timeStep * p.acceleration);
+	return disDiff;
 }
--- COMP5330M-GE/COMP5330M-GE/Particles.h
+++ COMP5330M-GE/COMP5330M-GE/Particles.h
@@ -11,6 +11,14 @@ struct Particle {
 	Particle(Vector3 initialDisplacement, Vector3 initialVelocity, int life);
 };
 
+struct ParticleBody {
+	Particle particle;
+	Vector3 acceleration;
+	float mass;
+
+	ParticleBody(Particle&, Vector3 acceleration, float mass);
+};
+
 struct Emitter {
 	Vector3 position;
 	float radius;
@@ -19,11 +27,21 @@ struct Emitter {
 	float maxAngleAroundX;
 	float maxAngleAroundY;
 	float maxAngleAroundZ;
+	// Has to be between 0.0 and 1.0
+	float minSpeedRatio;
 	int minLife;
 	int maxLife;
 
 	Emitter();
-	Emitter(Vector3 position, float radius, Vector3 normalSpeedVector, float rate, float maxAngleAroundX, float maxAngleAroundY, float maxAngleAroundZ, int minLife, int maxLife);
+	Emitter(Vector3 position, float radius, Vector3 normalSpeedVector, float rate, float maxAngleAroundX, float maxAngleAroundY, float maxAngleAroundZ, float minSpeedRatio, int minLife, int maxLife);
 };
 
 Particle releaseOneParticle(Emitter&);
+
+Particle simpleReleaseOneParticle(Emitter&);
+
+ParticleBody makeParticleRigidBody(Particle&, Vector3 acceleration, float mass);
+
+Vector3 updateParticlePosition(Particle&, float timeStep);
+
+Vector3 updateParticlePositionUnderForces(ParticleBody&, Vector3* forces, int numOfForces, float timeStep);
