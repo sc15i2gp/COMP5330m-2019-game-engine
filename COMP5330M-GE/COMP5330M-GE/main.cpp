8c22f618659986c882441e0a24d407cd0a0cdd8d
Daniel Banski <daniel.banski.1997@gmail.com>
2020-03-13 17:48:32 +0000

    Original main.cpp

--- COMP5330M-GE/COMP5330M-GE/main.cpp
+++ COMP5330M-GE/COMP5330M-GE/main.cpp
@@ -1,31 +1,11 @@
 #include "Platform.h"
 #include "Graphics.h"
 #include "Maths.h"
-
-#include<iostream>
 #include "Landscape.h"
 #include "l_system.h"
 #include "turtle.h"
 #include "Camera.h"
 #include "UI.h"
-
-//DOING:
-
-//TODO: 
-
-// Add a quad mesh with enough vertices
-// Add Sine Wave to imitiate waves
-// Figure out how the other type displacement happens
-// Add that displacement to the full thing
-// Add texture-mapping
-// Figure out howto add two textures simultaniously 
-// Make instances of this water quad
-
-//DEBUGGING FOR SHADERS
-
-//Triangle not rendering, fix the issues
-// Translate code as how Isaac has done it (?)
-
 
 Material gold =
 {
@@ -57,201 +37,191 @@ void buffer_camera_data_to_gpu(Camera c)
 	set_view_origin(c.position);
 }
 
+//Render 4*4 perlin noise grid on 64*64 texture
+Drawable buffer_heightmap_to_textured_quad(GLuint* heightmap_texture)
+{
+	Perlin_Noise_Function perlin_noise = generate_noise_function();
+	GLuint heightmap_data_width = 1024;
+	GLuint heightmap_data_height = 1024;
 
-float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
+	//Buffer texture data
+	GLfloat* heightmap_data = (GLfloat*)alloc_mem(heightmap_data_width*heightmap_data_height * sizeof(GLfloat));
+	float length = 128.0f;
+	float x_step = length / (float)heightmap_data_width;
+	float y_step = length / (float)heightmap_data_height;
+	//Render heightmap over range of terrain
+	for (int y = 0; y < heightmap_data_height; ++y)
+	{
+		for (int x = 0; x < heightmap_data_width; ++x)
+		{
+			float f_x = (float)x * x_step;
+			float f_y = (float)y * y_step;
+			float noise_val = (perlin_noise(f_x, f_y) + 1.0f) / 2.0f;
+			heightmap_data[y * heightmap_data_width + x] = noise_val;
+		}
+	}
+	*heightmap_texture = buffer_texture(heightmap_data_width, heightmap_data_height, heightmap_data, GL_RED);
+
+	//Create quad mesh
+	Mesh_vertex quad_vertices[4] = {};
+	//Top left
+	quad_vertices[0] =
+	{
+		{-1.0f, 1.0f, 0.0f}, //Position
+		{}, //Normal
+		{0.0f, 0.0f}  //Texture coords
+	};
+	//Bottom left
+	quad_vertices[1] =
+	{
+		{-1.0f, -1.0f, 0.0f}, //Position
+		{}, //Normal
+		{0.0f, 1.0f}  //Texture coords
+	};
+	//Bottom right
+	quad_vertices[2] =
+	{
+		{1.0f, -1.0f, 0.0f}, //Position
+		{}, //Normal
+		{1.0f, 1.0f}  //Texture coords
+	};
+	//Top right
+	quad_vertices[3] =
+	{
+		{1.0f, 1.0f, 0.0f}, //Position
+		{}, //Normal
+		{1.0f, 0.0f}  //Texture coords
+	};
+	GLuint quad_indices[] = { 0, 1, 2, 2, 3, 0 };
+	Drawable quad = buffer_mesh(quad_vertices, 4, quad_indices, 6);
+	dealloc_mem(heightmap_data);
+	return quad;
+}
+
+long int fps_to_mspf(int fps)
+{
+	float spf = 1.0f / (float)fps;
+	float mspf = 1000.0f*spf;
+	return (long int)mspf;
+}
+
+struct timer
+{
+	LARGE_INTEGER clock_frequency;
+	LARGE_INTEGER start_time;
+	LARGE_INTEGER stop_time;
 };
+void start_timer(timer* t)
+{
+	QueryPerformanceFrequency(&t->clock_frequency);
+	QueryPerformanceCounter(&t->start_time);
+}
 
+void stop_timer(timer* t)
+{
+	QueryPerformanceCounter(&t->stop_time);
+}
 
-//long int fps_to_mspf(int fps)
-//{
-//	float spf = 1.0f / (float)fps;
-//	float mspf = 1000.0f*spf;
-//	return (long int)mspf;
-//}
-//
-//struct timer
-//{
-//	LARGE_INTEGER clock_frequency;
-//	LARGE_INTEGER start_time;
-//	LARGE_INTEGER stop_time;
-//};
-//
-//void start_timer(timer* t)
-//{
-//	QueryPerformanceFrequency(&t->clock_frequency);
-//	QueryPerformanceCounter(&t->start_time);
-//}
-//
-//void stop_timer(timer* t)
-//{
-//	QueryPerformanceCounter(&t->stop_time);
-//}
-//
-//long int elapsed_time(timer* t)
-//{
-//	LARGE_INTEGER elapsed;
-//	elapsed.QuadPart = t->stop_time.QuadPart - t->start_time.QuadPart;
-//	elapsed.QuadPart *= 1000;
-//	elapsed.QuadPart /= t->clock_frequency.QuadPart;
-//
-//	return elapsed.QuadPart;
-//}
+long int elapsed_time(timer* t)
+{
+	LARGE_INTEGER elapsed;
+	elapsed.QuadPart = t->stop_time.QuadPart - t->start_time.QuadPart;
+	elapsed.QuadPart *= 1000;
+	elapsed.QuadPart /= t->clock_frequency.QuadPart;
+
+	return elapsed.QuadPart;
+}
 
 //Windows entry point
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmd_line, int nCmdShow)
 {
 	bool platform_ready = initialise_platform(instance);
 	bool graphics_ready = initialise_graphics();
-
-	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	const char* vertexShaderSource = read_file("testVertexShader.glsl");
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	// check for shader compile errors
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		OutputDebugStringA(infoLog);
-	}
-	// fragment shader
-	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	const char* fragmentShaderSource = read_file("testFragShader.glsl");
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	// check for shader compile errors
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		OutputDebugStringA(infoLog);
-	}
-	// link shaders
-	int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	// check for linking errors
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		OutputDebugStringA(infoLog);
-	}
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-
-	float vertices[] = {
-		0.9f,  0.9f, 0.0f,  // top right
-		0.9f, -0.9f, 0.0f,  // bottom right
-		-0.9f, -0.9f, 0.0f,  // bottom left
-		-0.9f,  0.9f, 0.0f   // top left 
-	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,  // first Triangle
-		1, 2, 3   // second Triangle
-	};
-
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-
-
-	glBindVertexArray(0);
-
-
-	/*Draws the surface in wire-frame mode*/
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	bool ui_ready = initialise_ui();
 
 	if (platform_ready && graphics_ready)
 	{
+		int bp_shader = load_shader_program("vshader.glsl", "fshader.glsl");
+		int terrain_shader = load_shader_program("vshader.glsl", "terrain_fshader.glsl");
+		int terrain_lighting_shader = load_shader_program("vshader.glsl", "fshader.glsl");
+		int heightmap_shader = load_shader_program("heightmap_vshader.glsl", "heightmap_fshader.glsl");
+
 		Camera main_view_camera = {};
-		main_view_camera.set_position_and_target(Vector3{ 0.0f, 0.0f, 0.0f }, Vector3{0.0f, 0.0f, 1.0f});
+		main_view_camera.set_position_and_target(Vector3{ 0.0f, 0.0f, 0.0f }, Vector3{ 0.0f, 0.0f, 1.0f });
 		main_view_camera.movement_sensitivity = 0.1f;
+		set_window_clear_colour(Vector3(0.52, 0.8, 0.92));
 
 		activate_direction_light(0);
 		set_direction_light_direction(0, Vector3(0.0f, -1.0f, -1.0f));
 		set_direction_light_blinn_phong_properties(0, light_properties);
 
+		Landscape_Data landscape = create_landscape(10.0f, 10.0f, 0.01f, 10);
+
 		bool dragging = false;
 		int fps = 60;
 		bool render_wireframes = false;
+		UI_Parameters ui_parameters = initialise_ui_parameter_pointers(&landscape, &main_view_camera, &fps, &render_wireframes);
 
-
-		while (!should_window_close()) 
+		timer t;
+		//Main loop
+		while (!should_window_close())
 		{
+			start_timer(&t);
+			long int mspf = fps_to_mspf(fps);
 
 			handle_input();
+			handle_ui(ui_parameters);
 
-			//if (was_mouse_button_pressed(BUTTON_LEFT) && was_mouse_moved() && !dragging)
-			//{
-			//	dragging = true;
-			//	main_view_camera.rotation_start(get_initial_mouse_position());
-			//}
-			//if (was_mouse_moved() && dragging)
-			//{
-			//	//main_view_camera.rotation_start(get_initial_mouse_position());
-			//	main_view_camera.rotate_by_arcball(get_final_mouse_position());
-			//}
-			//if (!was_mouse_button_pressed(BUTTON_LEFT) && dragging)
-			//{
-			//	main_view_camera.rotation_end();
-			//	dragging = false;
-			//}
+			if (was_mouse_button_pressed(BUTTON_LEFT) && was_mouse_moved() && !dragging)
+			{
+				dragging = true;
+				main_view_camera.rotation_start(get_initial_mouse_position());
+			}
+			if (was_mouse_moved() && dragging)
+			{
+				//main_view_camera.rotation_start(get_initial_mouse_position());
+				main_view_camera.rotate_by_arcball(get_final_mouse_position());
+			}
+			if (!was_mouse_button_pressed(BUTTON_LEFT) && dragging)
+			{
+				main_view_camera.rotation_end();
+				dragging = false;
+			}
 
-			//if (was_key_pressed(KEY_W)) main_view_camera.move_forward();
-			//if (was_key_pressed(KEY_A)) main_view_camera.move_left();
-			//if (was_key_pressed(KEY_S)) main_view_camera.move_backward();
-			//if (was_key_pressed(KEY_D)) main_view_camera.move_right();
-			//if (was_key_pressed(KEY_Q)) main_view_camera.move_up();
-			//if (was_key_pressed(KEY_E)) main_view_camera.move_down();
+			if (was_key_pressed(KEY_W)) main_view_camera.move_forward();
+			if (was_key_pressed(KEY_A)) main_view_camera.move_left();
+			if (was_key_pressed(KEY_S)) main_view_camera.move_backward();
+			if (was_key_pressed(KEY_D)) main_view_camera.move_right();
+			if (was_key_pressed(KEY_Q)) main_view_camera.move_up();
+			if (was_key_pressed(KEY_E)) main_view_camera.move_down();
 
-			glClearColor(131.0f / 255.0f, 122.0f / 255.0f, 122.0f / 255.0f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			begin_render();
+			if (render_wireframes) draw_as_wireframes();
+			else draw_as_polygons();
 
-			glUseProgram(shaderProgram);
-			glBindVertexArray(VAO);
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-
-			/*begin_render();*/
-
-		/*	set_projection_matrix(perspective(50.0f, get_window_aspect_ratio(), 0.1f, 1000.0f));
+			set_projection_matrix(perspective(50.0f, get_window_aspect_ratio(), 0.1f, 1000.0f));
 			set_model_matrix(identity());
-*/
-			//buffer_camera_data_to_gpu(main_view_camera);
+			use_shader(terrain_lighting_shader);
 
+			buffer_camera_data_to_gpu(main_view_camera);
+
+			//Draw landscape
+			landscape.draw();
+			render_ui();
 			swap_window_buffers();
+
+			stop_timer(&t);
+			long int frame_time = elapsed_time(&t);
+			if (frame_time < mspf)
+			{
+				DWORD sleep_time = mspf - frame_time;
+				Sleep(sleep_time);
+			}
 		}
-
-		/*Release resources before shut down*/
-		glDeleteVertexArrays(1, &VAO);
-		glDeleteBuffers(1, &VBO);
-		glDeleteBuffers(1, &EBO);
-
 		//release_drawable();
 		//ReleaseDC(window, window_device_context);
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplWin32_Shutdown();
 		shutdown_platform();
 		return 0;
 	}
