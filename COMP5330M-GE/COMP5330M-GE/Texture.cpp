9b47ac0a662e3c1c3f9997dbe2395d1a2e1bb79b
Daniel Banski <daniel.banski.1997@gmail.com>
2020-05-23 17:40:30 +0100

    Added Texture class, but not polished. Added image-loading header stb

---
+++ COMP5330M-GE/COMP5330M-GE/Texture.cpp
@@ -0,0 +1,28 @@
+#include "Texture.h"
+
+Texture::Texture(const std::string & filePath)
+{
+}
+
+void Texture::activeTexture()
+{
+}
+
+GLuint Texture::getTextureID() const
+{
+	return GLuint();
+}
+
+GLuint Texture::getTextureWidth() const
+{
+	return GLuint();
+}
+
+GLuint Texture::getTextureHieght() const
+{
+	return GLuint();
+}
+
+Texture::~Texture()
+{
+}
---
+++ COMP5330M-GE/COMP5330M-GE/Texture.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "OpenGL.h"
+#include<string>
+
+/*A texture class which enables us to read image files, and use them in our Game Engine*/
+class Texture
+{
+
+private:
+
+	GLuint m_textureWidth;
+	GLuint m_textureHeight;
+
+	GLuint m_textureID;
+
+public:
+
+	Texture(const std::string& filePath);
+
+	void activeTexture();
+
+	GLuint getTextureID() const;
+	GLuint getTextureWidth() const;
+	GLuint getTextureHieght() const;
+
+
+	~Texture();
+};
